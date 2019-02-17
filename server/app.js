f40b31f525d73c4d3ffce74ea5c31ca729e45115
Andrew Myint <doku.potharhtoo@gmail.com>
2019-02-17 14:53:31 -0500

    new session and cookies middleware created

--- server/app.js
+++ server/app.js
@@ -15,8 +15,8 @@ app.use(partials());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
-// app.use(parseCookie);
-// app.use(Auth.createSession);
+app.use(parseCookie);
+app.use(Auth.createSession);
 
 
 
